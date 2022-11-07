56bcee21b95da033b9ae8d26162181d890097064
tihitna <t.tsegaye@appdiv.com>
2022-11-07 09:16:55 +0300

    feat : email service added

---
+++ Application/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Application.Common.Models;
+
+namespace Application.Common.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
