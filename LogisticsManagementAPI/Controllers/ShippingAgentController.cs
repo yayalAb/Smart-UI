60a5cd6509da91903a668daf3a7c313a13b20aed
t.tsegaye <t.tsegaye@appdiv.com>
2022-11-16 14:16:34 +0300

    feat: read shipping agent list #1247

--- LogisticsManagementAPI/Controllers/ShippingAgentController.cs
+++ LogisticsManagementAPI/Controllers/ShippingAgentController.cs
@@ -2,6 +2,8 @@
 using Application.ShippingAgentModule.Commands.CreateShippingAgent;
 using Application.ShippingAgentModule.Commands.DeleteShippingAgent;
 using Application.ShippingAgentModule.Commands.UpdateShippingAgent;
+using Application.ShippingAgentModule.Queries.GetShippingAgentById;
+using Application.ShippingAgentModule.Queries.GetShippingAgentList;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,7 +12,27 @@ namespace WebApi.Controllers
 {
     public class ShippingAgentController : ApiControllerBase
     {
-       
+       // GET api/<ShippingAgentController>/
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetShippingAgentListQuery query)
+        {
+
+            var response = await Mediator.Send(query);
+            return Ok(response);
+        }
+
+        // GET api/<ShippingAgentController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var query = new GetShippingAgentByIdQuery
+            {
+                Id = id
+            };
+            var response = await Mediator.Send(query);
+            return Ok(response);
+        }
+
         // POST api/<ShippingAgentController>
         [HttpPost]
         public async Task<IActionResult> CreateShippingAgent ([FromForm] CreateShippingAgentCommand command)
