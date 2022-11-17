b0de3ec18033486f55859094721cfd9e6ce99a3d
Shambu-p <abnetkebede075@gmail.com>
2022-11-17 09:02:45 +0300

    setting crud backend #1255

---
+++ Domain/Entities/Setting.cs
@@ -0,0 +1,10 @@
+
+namespace Domain.Entities;
+
+public class Setting {
+    public string Email {get; set;}
+    public string Password {get; set;}
+    public string Port {get; set;}
+    public string Host {get; set;}
+    public string Protocol {get; set;}
+}
