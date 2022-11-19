e418fd4377babf7656c9bf42f5ee588476233bee
t.tsegaye <t.tsegaye@appdiv.com>
2022-11-19 10:22:51 +0300

    fix: group read changed to pagenated list

--- Application/UserGroupModule/Queries/GetUserGroupList/GetUserGroupList.cs
+++ Application/UserGroupModule/Queries/GetUserGroupList/GetUserGroupList.cs
@@ -1,17 +1,17 @@
-﻿
+
 using Application.Common.Interfaces;
+using Application.Common.Models;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace Application.UserGroupModule.Queries.GetAllUserGroups
+namespace Application.UserGroupModule.Queries.GetUserGroupList
 {
-    public record GetAllUserGroupsQuery : IRequest<IEnumerable<UserGroupDto>>
+    public record GetUserGroupListQuery : IRequest<List<UserGroupDto>>
     {
-
     }
-    public class GetAllUserGroupsCommandHandler : IRequestHandler<GetAllUserGroupsQuery, IEnumerable<UserGroupDto>>
+    public class GetAllUserGroupsCommandHandler : IRequestHandler<GetUserGroupListQuery, List<UserGroupDto>>
     {
         private readonly IAppDbContext _context;
         private readonly IMapper _mapper;
@@ -21,12 +21,11 @@ namespace Application.UserGroupModule.Queries.GetAllUserGroups
             _context = context;
             _mapper = mapper;
         }
-        public async Task<IEnumerable<UserGroupDto>> Handle(GetAllUserGroupsQuery request, CancellationToken cancellationToken)
+        public async Task<List<UserGroupDto>> Handle(GetUserGroupListQuery request, CancellationToken cancellationToken)
         {
             return await _context.UserGroups
                 .Include(ug => ug.UserRoles)
-                .ProjectTo<UserGroupDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+                .ProjectTo<UserGroupDto>(_mapper.ConfigurationProvider).ToListAsync();
         }
     }
 }
