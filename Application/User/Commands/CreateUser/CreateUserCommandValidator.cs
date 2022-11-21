9bbe84a553a105ba7fed8638870e2764f39cf1fb
t.tsegaye <t.tsegaye@appdiv.com>
2022-11-21 13:43:10 +0300

    fix: email validation

--- Application/User/Commands/CreateUser/CreateUserCommandValidator.cs
+++ Application/User/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -24,7 +24,8 @@ namespace Application.User.Commands.CreateUser
             RuleFor(u => u.Address.Email)
                 .NotEmpty()
                 .NotNull()
-                .EmailAddress();
+                .Matches("/^(([^<>()[\\]\\.,;:\\s@\"]+(\\.[^<>()[\\]\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/")
+                .WithMessage("invalid email address");
             RuleFor(u => u.UserGroupId)
                 .NotNull()
                 .NotEmpty()
