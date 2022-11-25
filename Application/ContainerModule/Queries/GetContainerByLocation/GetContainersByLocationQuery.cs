b989bbdc77db92a2a78cb8ed18ab681d66a4aae4
t.tsegaye <t.tsegaye@appdiv.com>
2022-11-25 17:36:37 +0300

    feat : get container by location

--- Application/ContainerModule/Queries/GetContainerByLocation/GetContainersByLocationQuery.cs
+++ Application/ContainerModule/Queries/GetContainerByLocation/GetContainersByLocationQuery.cs
@@ -5,34 +5,57 @@ using Microsoft.EntityFrameworkCore;
 using AutoMapper.QueryableExtensions;
 using AutoMapper;
 using Application.Common.Models;
+using Application.Common.Exceptions;
 
 namespace Application.ContainerModule.Queries.GetContainersByLocationQueryQuery;
 
-public class GetContainersByLocationQuery : IRequest<List<ContainerDto>> {
-    public int OperationId {set; get;}
-    public string Location {get; set;}
+public class GetContainersByLocationQuery : IRequest<List<ContainerDto>>
+{
+    public int? OperationId { set; get; }
+    public string? Location { get; set; }
 }
-public class GetContainersByLocationQueryHandler: IRequestHandler<GetContainersByLocationQuery, List<ContainerDto>> {
+public class GetContainersByLocationQueryHandler : IRequestHandler<GetContainersByLocationQuery, List<ContainerDto>>
+{
 
     private readonly IIdentityService _identityService;
     private readonly IAppDbContext _context;
     private readonly IMapper _mapper;
 
     public GetContainersByLocationQueryHandler(
-        IIdentityService identityService, 
+        IIdentityService identityService,
         IAppDbContext context,
         IMapper mapper
-    ){
+    )
+    {
         _identityService = identityService;
         _context = context;
         _mapper = mapper;
     }
 
-    public async Task<List<ContainerDto>> Handle(GetContainersByLocationQuery request, CancellationToken cancellationToken) {
-       return await _context.Containers
-            .Where(c => c.OperationId == request.OperationId && c.Location == request.Location)
-            .Include(c => c.Goods)
-            .ProjectTo<ContainerDto>(_mapper.ConfigurationProvider).ToListAsync();
+    public async Task<List<ContainerDto>> Handle(GetContainersByLocationQuery request, CancellationToken cancellationToken)
+    {
+        if (request.OperationId != null && request.Location != null)
+        {
+            return await _context.Containers
+                        .Where(c => c.OperationId == request.OperationId && c.Location == request.Location)
+                        .Include(c => c.Goods)
+                        .ProjectTo<ContainerDto>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+        if (request.OperationId == null && request.Location != null)
+        {
+            return await _context.Containers
+                        .Where(c => c.Location == request.Location)
+                        .Include(c => c.Goods)
+                        .ProjectTo<ContainerDto>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+        if(request.OperationId !=null && request.Location == null){
+            return await _context.Containers
+                        .Where(c => c.OperationId == request.OperationId)
+                        .Include(c => c.Goods)
+                        .ProjectTo<ContainerDto>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+       throw new GhionException(CustomResponse.BadRequest("atleast one query param must be provided for filtering"));
+
     }
 
 }
