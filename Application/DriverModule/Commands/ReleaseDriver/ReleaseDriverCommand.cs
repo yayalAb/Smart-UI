e802ccc04e74310dd0bafccb1c53e69723ffa86f
t.tsegaye <t.tsegaye@appdiv.com>
2022-11-29 16:30:55 +0300

    feat: release driver and truck done

---
+++ Application/DriverModule/Commands/ReleaseDriver/ReleaseDriverCommand.cs
@@ -0,0 +1,35 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.DriverModule.Commands.ReleaseDriver;
+public record ReleaseDriverCommand : IRequest<CustomResponse>
+{
+    public int Id { get; set; }
+}
+public class ReleaseDriverCommandHandler : IRequestHandler<ReleaseDriverCommand, CustomResponse>
+{
+    private readonly IAppDbContext _context;
+
+    public ReleaseDriverCommandHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<CustomResponse> Handle(ReleaseDriverCommand request, CancellationToken cancellationToken)
+    {
+        var driver = await _context.Drivers.FindAsync(request.Id);
+        if (driver == null)
+        {
+            throw new GhionException(CustomResponse.NotFound($"driver with id = {request.Id} is not found"));
+
+        }
+        if(!driver.IsAssigned){
+            return CustomResponse.Succeeded("driver is already unassigned");
+        }
+        driver.IsAssigned = false;
+        _context.Drivers.Update(driver);
+        await _context.SaveChangesAsync(cancellationToken);
+        return CustomResponse.Succeeded("driver released  successfully!");
+    }
+}
---
+++ Application/TruckModule/Commands/ReleaseTruck/ReleaseTruckCommand.cs
@@ -0,0 +1,35 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.TruckModule.Commands.ReleaseTruck;
+public record ReleaseTruckCommand : IRequest<CustomResponse>
+{
+    public int Id { get; set; }
+}
+public class ReleaseTruckCommandHandler : IRequestHandler<ReleaseTruckCommand, CustomResponse>
+{
+    private readonly IAppDbContext _context;
+
+    public ReleaseTruckCommandHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<CustomResponse> Handle(ReleaseTruckCommand request, CancellationToken cancellationToken)
+    {
+        var Truck = await _context.Trucks.FindAsync(request.Id);
+        if (Truck == null)
+        {
+            throw new GhionException(CustomResponse.NotFound($"Truck with id = {request.Id} is not found"));
+
+        }
+        if(!Truck.IsAssigned){
+            return CustomResponse.Succeeded("Truck is already unassigned");
+        }
+        Truck.IsAssigned = false;
+        _context.Trucks.Update(Truck);
+        await _context.SaveChangesAsync(cancellationToken);
+        return CustomResponse.Succeeded("Truck released  successfully!");
+    }
+}
