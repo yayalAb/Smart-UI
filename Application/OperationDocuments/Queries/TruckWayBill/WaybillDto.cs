179bf11664ad0ccb92ec5b8a1cc89b29f3cf4865
t.tsegaye <t.tsegaye@appdiv.com>
2022-12-07 17:15:14 +0300

    fix: waybill dto updated

--- Application/OperationDocuments/Queries/TruckWayBill/WaybillDto.cs
+++ Application/OperationDocuments/Queries/TruckWayBill/WaybillDto.cs
@@ -7,5 +7,8 @@ public class WaybillDto : DocsDto
     public string? DeclarationNumber { get; set; }
     public string? DriverName { get; set; }
     public string? DriverPhone { get; set; }
+    public string? DriverLicenceNumber { get; set; }
+    public string? TruckNumber { get; set; }
+    public string? PlateNumber { get; set; }
     
 }
