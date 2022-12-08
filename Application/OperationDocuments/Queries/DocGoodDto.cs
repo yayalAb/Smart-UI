5d23a0c9883c8cc9569ee17e7918bffc3ea92d65
t.tsegaye <t.tsegaye@appdiv.com>
2022-12-08 10:48:37 +0300

    fix: default company info in no1 added

--- Application/OperationDocuments/Queries/DocGoodDto.cs
+++ Application/OperationDocuments/Queries/DocGoodDto.cs
@@ -9,10 +9,11 @@ public class DocGoodDto : IMapFrom<Good>
     public string? HSCode { get; set; }
     public string? Manufacturer { get; set; }
     public float Weight { get; set; }
-    public float? Quantity { get; set; }
     public int NumberOfPackages { get; set; }
     public string Type { get; set; }
     public string? Location {get; set;}
+    public string? Unit { get; set; }
+    public float? UnitPrice { get; set; }
     public string? ChasisNumber { get; set; }
     public string? EngineNumber { get; set; }
     public string? ModelCode { get; set; }
