4a69c5daa38bb60cb9d70648dd5ae16b7acd2a98
t.tsegaye <t.tsegaye@appdiv.com>
2022-12-14 12:07:39 +0300

    fix: contact person dto fixed

--- Application/CompanyModule/Queries/ContactPersonDto.cs
+++ Application/CompanyModule/Queries/ContactPersonDto.cs
@@ -4,8 +4,11 @@ using Domain.Entities;
 namespace Application.CompanyModule.Queries;
 public class ContactPersonDto : IMapFrom<ContactPerson>
 {
-    public int Id {get; set;}
+    public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string? Email { get; set; }
     public string? Phone { get; set; }
+    public string TinNumber { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string City { get; set; } = null!;
 }
