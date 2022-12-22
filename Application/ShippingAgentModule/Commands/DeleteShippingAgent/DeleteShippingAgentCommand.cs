fe91650d44cb1acb6be440970d573f388667aeff
t.tsegaye <t.tsegaye@appdiv.com>
2022-12-22 17:30:56 +0300

    fix: address delete bug fixed

--- Application/ShippingAgentModule/Commands/DeleteShippingAgent/DeleteShippingAgentCommand.cs
+++ Application/ShippingAgentModule/Commands/DeleteShippingAgent/DeleteShippingAgentCommand.cs
@@ -4,6 +4,7 @@ using Application.Common.Exceptions;
 using Application.Common.Interfaces;
 using Application.Common.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.ShippingAgentModule.Commands.DeleteShippingAgent
 {
@@ -21,12 +22,16 @@ namespace Application.ShippingAgentModule.Commands.DeleteShippingAgent
         }
         public async Task<CustomResponse> Handle(DeleteShippingAgentCommand request, CancellationToken cancellationToken)
         {
-            var found_ShippingAgent = await _context.ShippingAgents.FindAsync(request.Id);
+            var found_ShippingAgent = await _context.ShippingAgents
+                            .Include(sa => sa.Address)
+                            .Where(sa => sa.Id == request.Id).FirstOrDefaultAsync();
             if (found_ShippingAgent == null)
             {
                 throw new GhionException(CustomResponse.NotFound($"ShippingAgent with id = {request.Id} is not found"));
             }
+            var shippingAgentAddress = found_ShippingAgent.Address;
             _context.ShippingAgents.Remove(found_ShippingAgent);
+            _context.Addresses.Remove(shippingAgentAddress);
             await _context.SaveChangesAsync(cancellationToken);
 
             return CustomResponse.Succeeded("ShippingAgent deleted successfully!");
