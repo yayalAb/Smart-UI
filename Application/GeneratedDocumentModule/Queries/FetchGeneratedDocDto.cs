adf63492837a3b7ecd5fc24ac4c3565a579fc934
t.tsegaye <t.tsegaye@appdiv.com>
2022-12-23 10:08:12 +0300

    fix: generated doc list dto updated

--- Application/GeneratedDocumentModule/Queries/FetchGeneratedDocDto.cs
+++ Application/GeneratedDocumentModule/Queries/FetchGeneratedDocDto.cs
@@ -13,9 +13,9 @@ public class FetchGeneratedDocumentDto : IMapFrom<GeneratedDocument>
     public string DocumentType { get; set; } /// t1 , no.1....
     public int OperationId { get; set; }
     public DateTime GeneratedDate {get; set; }
-    public OperationDto2 Operation { get; set; }
+    // public OperationDto2 Operation { get; set; }
     public ContactPersonDto ContactPerson { get; set; }
     public PortDto DestinationPort { get; set; }
-    public ICollection<ContainerDto2> Containers { get; set; }
-    public ICollection<GeneratedDocumentsGoodsDto> GeneratedDocumentsGoods {get; set; }
+    // public ICollection<ContainerDto2> Containers { get; set; }
+    // public ICollection<GeneratedDocumentsGoodsDto> GeneratedDocumentsGoods {get; set; }
 }
